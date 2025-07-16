04c6577cd613372e1aa3d49b823256ce4050e0a1
crshbot3000 <pauljrthelen58@gmail.com>
2025-07-16 05:07:25 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,14 @@
-// pages/index.js
-import React, { useState } from 'react';
-import ProfitDisplay from '../components/ProfitDisplay';
-import { ethers } from 'ethers';
+import React from 'react';
+import WalletConnectButton from '../components/WalletConnectButton';
 
 export default function Home() {
-  const [account, setAccount] = useState(null);
-
-  const connectWallet = async () => {
-    if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
-      try {
-        const provider = new ethers.BrowserProvider(window.ethereum);
-        const accounts = await provider.send('eth_requestAccounts', []);
-        setAccount(accounts[0]);
-      } catch (err) {
-        console.error('Wallet connection failed:', err);
-      }
-    } else {
-      alert('Please install MetaMask!');
-    }
-  };
-
   return (
     <div style={{ padding: '2rem' }}>
       <h1>🚀 Welcome to Money Machine</h1>
       <p>This system is live and auto-optimized.</p>
 
-      <button onClick={connectWallet
+      {/* Wallet Button Here */}
+      <WalletConnectButton />
+    </div>
+  );
+}
