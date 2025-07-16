effb59d8e068c416700586be0f835ffe11830607
crshbot3000 <pauljrthelen58@gmail.com>
2025-07-16 03:09:47 -0400

    Create WalletConnectButton.js

---
+++ components/WalletConnectButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function WalletConnectButton() {
+  return (
+    <button style={{ padding: '10px', fontSize: '16px' }}>
+      Connect Wallet
+    </button>
+  );
+}
